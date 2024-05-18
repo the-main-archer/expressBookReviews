ed9ca8bbe77907db24c77318484faa431c6c1a12
Terrence <the.main.archer@gmail.com>
2024-05-18 14:39:14 -0400

    finished final project for this course

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,21 +9,54 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let filtered_book = books[isbn]
+  if (filtered_book) {
+      let review = req.query.reviews;
+      let reviewer = req.session.authorization['username'];
+      if(review) {
+          filtered_book['reviews'][reviewer] = review;
+          books[isbn] = filtered_book;
+      }
+      res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
+  }
+  else{
+      res.send("Unable to find this ISBN!");
+  }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+    let filtered_book = books[isbn];
+
+    if (filtered_book) {
+        let reviewer = req.session.authogitrization ? req.session.authorization['username'] : null;
+
+        if (!reviewer) {
+            return res.status(403).json({ message: 'User not authenticated.' });
+        }
+
+        let filtered_review = filtered_book.reviews;
+
+        if (filtered_review && filtered_review[reviewer]) {
+            delete filtered_review[reviewer];
+            res.send(`Review for the ISBN ${isbn} posted by the user ${reviewer} deleted.`);
+        } else {
+            res.status(403).json({ message: "Can't delete, as this review has been posted by a different user or does not exist." });
+        }
+    } else {
+        res.status(404).json({ message: 'Unable to find this ISBN!' });
+    }
+});
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,38 +5,107 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
+
+
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const get_books = new Promise ((resolve, reject) => {
+      resolve(res.send(JSON.stringify(books,null,4)));
+  });
+  get_books.
+      then(function(){
+        console.log("Promise is resolved");
+      }).
+      catch(function(){
+        console.log("Could not locate book");
+      });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const get_books_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+
+    if (req.params.isbn <=10) {
+      resolve(res.send(books[isbn]));
+    }
+
+    else {
+      reject(res.send('Could not locate ISBN'));
+    }
+  });
+  get_books_isbn.
+        then(function(){
+            console.log("Promise is resolved");
+   }).
+        catch(function () { 
+                console.log('Could not locate ISBN');
+  });
+
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const get_books_author = new Promise((resolve, reject) => {
+
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+      }
+
+
+    });
+    reject(res.send("The mentioned author does not exist "))
+        
+    });
+
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
+  });
+
+  });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const get_books_title = new Promise((resolve, response) => {
+
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+        "title":books[isbn]["title"],
+        "reviews":books[isbn]["reviews"]});
+      }
+    });
+
+    resolve(res.send(JSON.stringify({booksbytitle},null,4)));
+    reject(res.send("Cannot locate a book with this title"))
+  });
+
+  get_books_title.then(function(){
+    console.log("Promise is resolved");
+  }).catch(function(){
+    console.log("Cannot locate a book with this title");
+  });
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+    const review = req.params.isbn;
+    res.send(books[review]);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
